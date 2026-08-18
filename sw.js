c66453044fb8dfd85a8e0166b9367c1e5ed8c1ad
Zazacorp2026 <jorgemoraloza@gmail.com>
2026-08-18 15:06:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zazafoto-v12';
+const CACHE = 'zazafoto-v13';
 const PRECACHE = ['./index.html', './manifest.json', './icon/icon-192.png', './icon/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(
